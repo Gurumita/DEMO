4a172d1c9e995972c40ae025a28a123de779a630
Gurumita <140096736+Gurumita@users.noreply.github.com>
2024-08-12 06:22:41 +0530

    Delete IdeaProjects/Quiz/Quiz directory

--- IdeaProjects/Quiz/Quiz/src/main/java/com/Quiz/Quiz/Exceptions/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.Quiz.Quiz.Exceptions;
-
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
-
--- IdeaProjects/Quiz/Quiz/src/main/java/com/Quiz/Quiz/Repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Quiz.Quiz.Repository;
-
-import com.Quiz.Quiz.Models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
